ba6273e65f1be19f0479617ae0fcf9a1e5a6dae6
pooky1225 <geometry1224gd@gmail.com>
2022-07-27 19:11:20 -0700

    Add source code

---
+++ patch.py
@@ -0,0 +1,32 @@
+import os
+from os import path
+import requests
+cwd = os.getcwd()
+path = path.expandvars(r'%localappdata%\Roblox\Versions')
+print('roblox path')
+print(path)
+isExist = os.path.exists(path)
+print('Does it exist?')
+print(isExist)
+print(os.listdir(path))
+cwd = os.chdir(path)
+ver = input('input the version-xxx folder and press <enter>')
+print(ver)
+verc = '\\'+ver
+pathver = path+verc
+finpath = pathver+r'\content\sounds'
+oggpath = finpath+r'ouch.ogg'
+print('downloading to')
+print(finpath)
+cwd = os.chdir(finpath)
+os.rename('ouch.ogg','ouch.ogg.old')
+url = "https://cdn.discordapp.com/attachments/900063420882255912/1002024528442036344/ouch.ogg"
+r = requests.get(url)
+with open('ouch.ogg','wb') as outfile:
+    outfile.write(r.content)
+print('delete ouch.ogg.old')
+os.remove("ouch.ogg.old")
+print('opening file explorer')
+os.startfile(finpath)
+input('finished, press <enter> to exit')
+
